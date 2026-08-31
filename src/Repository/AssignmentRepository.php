0171c74cb451b8a0772cd870f6e465f0d1e1dc34
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-08-31 11:23:30 +0330

    feat(repository): add AssignmentRepository with initial functions

--- src/Repository/AssignmentRepository.php
+++ src/Repository/AssignmentRepository.php
@@ -3,6 +3,7 @@
 namespace TM\Repository;
 
 use PDO;
+use TM\Model\Assignment;
 
 class AssignmentRepository {
     public function __construct(
@@ -10,6 +11,38 @@ class AssignmentRepository {
     ){}
 
     public function findByUserId(int $id): array {
-        $sql = "SELECT "
+        $assignments = [];
+        $sql = "SELECT id, user_id, task_id FROM assignments WHERE user_id = :user_id";
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute(['user_id' => $id]);
+        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($rows as $row) {
+            $this->mapToAssignemnt($row);
+        }
+
+        return $assignments;
+    }
+
+    public function finByTaskId(int $id): array {
+        $assignments = [];
+        $sql = "SELECT id, user_id, task_id FROM assignments WHERE task_id = :task_id";
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute(['task_id' => $id]);
+        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($rows as $row) {
+            $this->mapToAssignemnt($row);
+        }
+
+        return $assignments;
+    }
+
+    private function mapToAssignemnt(array $row): Assignment {
+        return new Assignment(
+            (int) $row['id'],
+            (int) $row['user_id'],
+            (int) $row['task_id']
+        );
     }
 }
