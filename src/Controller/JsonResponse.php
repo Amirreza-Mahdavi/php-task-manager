59d9a807c72d10946fc8d122e476aed1ead0c608
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-09-02 12:57:19 +0330

    feat(controller): add JsonResponse class

---
+++ src/Controller/JsonResponse.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TM\Controller;
+
+class JsonResponse {
+    public function __construct(
+        private mixed $data,
+        private int $statusCode = 200
+    ){}
+
+    public function send(): void {
+        http_response_code($this->statusCode);
+        header('Content-Type: application/json');
+        echo json_decode($this->data);
+    }
+}
