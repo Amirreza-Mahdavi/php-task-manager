4b41da7f0af19f7c160b9fe88c24bb99e6229bf5
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-09-03 17:39:07 +0330

    fix(json-request-trait): handle invalis JSON exception

--- src/Trait/JsonRequestTrait.php
+++ src/Trait/JsonRequestTrait.php
@@ -2,8 +2,14 @@
 
 namespace TM\Trait;
 
+use RuntimeException;
+
 trait JsonRequestTrait {
     public function getJsonBody(): array {
-        return json_decode(file_get_contents('php://input'), true);
+        $data = json_decode(file_get_contents('php://input'), true);
+        if (!is_array($data))
+            throw new RuntimeException("Invalid JSON");
+
+        return $data;
     }
 }
