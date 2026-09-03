280fc0d8dbbb6106edc103dcc89697d5a4b4e7fc
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-09-03 17:17:06 +0330

    add email uniqueness to database schema

--- database/schema.sql
+++ database/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
     role_id BIGINT NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
 
     FOREIGN KEY (role_id) REFERENCES roles(id)
